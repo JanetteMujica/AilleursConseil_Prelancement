7f999f7dc9fe156849aabd8d6d9427c446b4308f
“Janette <mujica.janette@gmail.com>
2022-12-12 14:05:25 -0500

    Setup of app.js and GlobalStyles

---
+++ client/src/data.js
@@ -0,0 +1,19 @@
+export const titles = [
+	IDENTITE,
+	PROJET,
+	RITUEL,
+	GOUVERNANCE,
+	ECOSYSTEME,
+	IMPACT,
+];
+
+export const modules = [
+	Identité,
+	Projet,
+	Rituel,
+	Gouvernance,
+	Écosystème,
+	Impact,
+];
+
+export const AllModules = [];
